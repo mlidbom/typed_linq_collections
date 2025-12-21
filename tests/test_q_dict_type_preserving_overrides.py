18766890522866c91ff2d352cfe1dfc52ecaa3d2
Magnus Lidbom <magnus.lidbom@gmail.com>
2025-12-21 11:02:22 +0100

    Add typing to the tests so that we will find out if pyright is confused by anything in our code.

--- tests/test_q_dict_type_preserving_overrides.py
+++ tests/test_q_dict_type_preserving_overrides.py
@@ -1,43 +1,45 @@
 from __future__ import annotations
 
+from typing import cast
+
 from typed_linq_collections.collections.q_dict import QDict
 
 
 # Merge operators
 def test_or_operator_returns_qdict_with_the_correct_values() -> None:
-    result = QDict([(1, "a"), (2, "b")]) | {3: "c", 4: "d"}
+    result: QDict[int, str] = cast(QDict[int, str], QDict([(1, "a"), (2, "b")]) | {3: "c", 4: "d"})
     assert isinstance(result, QDict)
     assert result == {1: "a", 2: "b", 3: "c", 4: "d"}
 
 
 def test_or_operator_overwrites_with_right_side_values() -> None:
-    result = QDict([(1, "a"), (2, "b")]) | {2: "B", 3: "c"}
+    result: QDict[int, str] = cast(QDict[int, str], QDict([(1, "a"), (2, "b")]) | {2: "B", 3: "c"})
     assert isinstance(result, QDict)
     assert result == {1: "a", 2: "B", 3: "c"}
 
 
 def test_reverse_or_operator_returns_qdict_with_the_correct_values() -> None:
-    result = {1: "a", 2: "b"} | QDict([(3, "c"), (4, "d")])
+    result: QDict[int, str] = cast(QDict[int, str], {1: "a", 2: "b"} | QDict([(3, "c"), (4, "d")]))
     assert isinstance(result, QDict)
     assert result == {1: "a", 2: "b", 3: "c", 4: "d"}
 
 
 def test_reverse_or_operator_overwrites_with_right_side_values() -> None:
-    result = {1: "a", 2: "b"} | QDict([(2, "B"), (3, "c")])
+    result: QDict[int, str] = cast(QDict[int, str], {1: "a", 2: "b"} | QDict([(2, "B"), (3, "c")]))
     assert isinstance(result, QDict)
     assert result == {1: "a", 2: "B", 3: "c"}
 
 
 # In-place operators
 def test_inplace_or_operator_returns_qdict_with_the_correct_values() -> None:
-    qd = QDict([(1, "a"), (2, "b")])
+    qd: QDict[int, str] = QDict([(1, "a"), (2, "b")])
     qd |= {3: "c", 4: "d"}
     assert isinstance(qd, QDict)
     assert qd == {1: "a", 2: "b", 3: "c", 4: "d"}
 
 
 def test_inplace_or_operator_overwrites_with_right_side_values() -> None:
-    qd = QDict([(1, "a"), (2, "b")])
+    qd: QDict[int, str] = QDict([(1, "a"), (2, "b")])
     qd |= {2: "B", 3: "c"}
     assert isinstance(qd, QDict)
     assert qd == {1: "a", 2: "B", 3: "c"}
@@ -45,6 +47,6 @@ def test_inplace_or_operator_overwrites_with_right_side_values() -> None:
 
 # Methods
 def test_copy_returns_qdict_with_the_correct_values() -> None:
-    result = QDict([(1, "a"), (2, "b")]).copy()
+    result: QDict[int, str] = cast(QDict[int, str], QDict([(1, "a"), (2, "b")]).copy())
     assert isinstance(result, QDict)
     assert result == {1: "a", 2: "b"}
